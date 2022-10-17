936903f84a1c5357efe23ed051c7cfbffc81350c
Name <git@mail.123com>
2022-10-17 23:58:46 +0300

    request

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import http from 'http';
 
 http.createServer((req,res)=> {
-    req.on('data', () => {
-        res.end('response123');
-    })
+    console.log(req.url);
+    console.log(req.headers["user-agent"]);
+    let body: any[] | Uint8Array[] = [];
+    req.on('data', (chunk) =>{
+        body.push(chunk);
+    }).on('data', () => {
+        let data = Buffer.concat(body).toString();
+        console.log(data);
+        res.statusCode = 200;
+        res.end(data);        
+    }) 
 }).listen(3000, () => {
     console.log(` Server is running`)
 })
